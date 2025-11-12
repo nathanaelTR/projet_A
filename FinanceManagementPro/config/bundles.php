c2da93345141a7a0a3be99a848eb18207c8e12fb
TRNathanael <trouart.nathanael.pro@gmail.com>
2025-11-12 12:12:49 +0100

    Add Tailwing + Login User

--- FinanceManagementPro/config/bundles.php
+++ FinanceManagementPro/config/bundles.php
@@ -7,4 +7,8 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
