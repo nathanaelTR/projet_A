0d1ecd111e36ec78a045d2cd38136ec5779aa3c7
TRNathanael <trouart.nathanael.pro@gmail.com>
2025-11-18 10:37:15 +0100

    Package.json and Resolution Conflics

--- FinanceManagementPro/config/bundles.php
+++ FinanceManagementPro/config/bundles.php
@@ -11,11 +11,7 @@ return [
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
-<<<<<<< HEAD
     Scheb\TwoFactorBundle\SchebTwoFactorBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-=======
-    Symfony\UX\Chartjs\ChartjsBundle::class => ['all' => true],
->>>>>>> front-dev
 ];
